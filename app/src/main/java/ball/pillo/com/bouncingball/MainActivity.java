2d5f827a8053cadcb5b2b33c598ffbca4f2a7147
Taylor Courtney <tcourtney@pillocompany.com>
2015-11-18 21:52:35 -0500

    Initial commit

---
+++ app/src/main/java/ball/pillo/com/bouncingball/MainActivity.java
@@ -0,0 +1,82 @@
+package ball.pillo.com.bouncingball;
+
+
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Phil, define the circle
+    //import the game engine with Gradle
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //here is the layout you will be pulling from
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+       //look at res activity_main or content_main and make the background a special color or image
+
+        //Stephen, here is your button to make the circle "jump"
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //Phil, here is what happens when someone clicks on the button
+
+
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    //below is stuff you dont really need to worry about
+    //it is the actionbar or tabbar or navigationbar or whatever you want to call it
+
+
+    //this first one creates the action bar
+    //if you go to res menu you can find the menu it is putting into the actionbar
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //here is what happens when anything in the menu/actionbar or whatever you wanna call it is clicked
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        //super. is important it means that whatever usually happens does, basically
+
+        return super.onOptionsItemSelected(item);
+    }
+}
